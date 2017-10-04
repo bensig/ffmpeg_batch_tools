55cb4d444499cffeb9b67fd109ff22016423edf8
Ben Sigman <bensig@gmail.com>
2017-10-04 06:25:39 -0400

    changed some variables, simplified

--- tc-burn.sh
+++ tc-burn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #This script will burn timecode from what is embedded in a video file
 #in=/path/to/input_file
-in=11000001.MP4
+in="11000001.MP4"
 echo -e "$in"
 #out=/path/to/ouput_file.mov
 out="${in%.*}.mov"
@@ -9,16 +9,18 @@ echo -e "$out"
 #TC rate should be identical to FPS
 tcrate=24
 #font size and position
-font=/usr/local/share/fonts/d/DroidSansMono_Regular.ttf
+font="/usr/local/share/fonts/d/DroidSansMono_Regular.ttf"
 fontsize=22
 # time code position bottom center:
 position="x=(w-tw)/2: y=h-(2*lh)"
 # get the timecode, and escape the ":" to be able to use it in the burn-in filter
 tcode=$( ffmpeg -i "$in" 2>&1 | awk '$1 ~ /^timecode/ {print $NF}' )
 tc_escaped=${tcode//:/\\:}
+#scaling
+scaling="scale=w=iw/4:h=ih/4"
 #make this slower to increase quality over speed
 preset=ultrafast
 #remove audio
-noaudio=-an
-# And finally
-ffmpeg -threads 0 -i "$in" $test_secs -pix_fmt yuvj422p -map 0:0 -map 0:1 $noaudio -c:v mjpeg -q:v 3 -huffman optimal -preset $preset -deinterlace -vf "scale=w=iw/4:h=ih/4,drawtext=fontfile=$font: timecode='$tc_escaped': r=$tcrate: $position: fontcolor=white: fontsize=$fontsize: box=1: boxcolor=black@0.2" "$out"
+noaudio="-an"
+# And finally run the ffmpeg script
+ffmpeg -threads 0 -i $in $noaudio -pix_fmt yuvj422p -c:v mjpeg -q:v 3 -huffman optimal -preset $preset -deinterlace -vf "$scaling,drawtext=fontfile=$font: timecode='$tc_escaped': r=$tcrate: $position: fontcolor=white: fontsize=$fontsize: box=1: boxcolor=black@0.2" "$out"
