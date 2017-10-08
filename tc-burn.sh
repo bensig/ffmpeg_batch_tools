3f172114a6fde885bd2b91b8b71e7e98943536da
Ben Sigman <bensig@gmail.com>
2017-10-08 00:01:10 -0700

    made variables more descriptive and added some variables

--- tc-burn.sh
+++ tc-burn.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 #This script will burn timecode from what is embedded in a video file
-#in=/path/to/input_file
-read -e -p "Enter path to input file:" in
-echo -e "$in"
+#in_file_path=/path/to/input_file
+read -e -p "Enter path to input file:" in_file_path
+echo -e "$in_file_path"
 #out=/path/to/ouput_file.mov
-out="${in%.*}.mov"
-echo -e "$out"
+out_file_path="${in_file_path%.*}.mov"
+echo -e "$out_file_pah"
 #TC rate should be identical to FPS
-tcrate=24
+timecode_rate=24
 #font size and position
-font="/usr/local/share/fonts/d/DroidSansMono_Regular.ttf"
-fontsize=22
+timecode_font="/usr/local/share/fonts/d/DroidSansMono_Regular.ttf"
+timecode_font_size=22
+timecode_font_color=white
+timecode_box_color='black@0.2'
 # time code position bottom center:
-position="x=(w-tw)/2: y=h-(2*lh)"
+timecode_position="x=(w-tw)/2: y=h-(2*lh)"
 # get the timecode, and escape the ":" to be able to use it in the burn-in filter
-tcode=$( ffmpeg -i "$in" 2>&1 | awk '$1 ~ /^timecode/ {print $NF}' )
-tc_escaped=${tcode//:/\\:}
+timecode_subprocess=$( ffmpeg -i "$in_file_path" 2>&1 | awk '$1 ~ /^timecode/ {print $NF}' )
+timecode_subprocess__timecode_formatted=${timecode_subprocess//:/\\:}
 #scaling
-scaling="scale=w=iw/4:h=ih/4"
+video_resolution_scaling="scale=w=iw/4:h=ih/4"
+video_endcoder='-pix_fmt yuvj422p -c:v mjpeg -huffman optimal'
+video_variable_bitrate='-q:v 3'
 #make this slower to increase quality over speed
-preset=ultrafast
+video_encoder_preset='ultrafast'
 #remove audio
-noaudio="-an"
+audio_encoder_option='-an'
 # And finally run the ffmpeg script
-ffmpeg -threads 0 -i $in $noaudio -pix_fmt yuvj422p -c:v mjpeg -q:v 3 -huffman optimal -preset $preset -deinterlace -vf "$scaling,drawtext=fontfile=$font: timecode='$tc_escaped': r=$tcrate: $position: fontcolor=white: fontsize=$fontsize: box=1: boxcolor=black@0.2" "$out"
+ffmpeg -threads 0 -i $in_file_path $audio_encoder_option $video_endcoder $video_variable_bitrate -preset $video_encoder_preset -deinterlace -vf "$video_resolution_scaling,drawtext=fontfile=$timecode_font: timecode='$timecode_subprocess__timecode_formatted': r=$timecode_rate: $timecode_position: fontcolor=$timecode_font_color: fontsize=$timecode_font_size: box=1: boxcolor=$timecode_box_color" "$out_file_path"
