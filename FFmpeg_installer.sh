5a1ffad826b601c1706eae646a5503668359384c
Ben Sigman <bensig@gmail.com>
2017-10-20 17:34:06 -0400

    Added some extra packages for static compiling to FFmpeg_installer.sh

--- FFmpeg_installer.sh
+++ FFmpeg_installer.sh
@@ -4,6 +4,24 @@
 mkdir ~/ffmpeg_sources
 sudo apt-get remove ffmpeg 
 sudo apt-get update && sudo apt-get install --no-upgrade libtheora-dev libass-dev libfreetype6-dev autoconf autogen automake build-essential libsdl2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config texinfo wget zlib1g-dev yasm libx264-dev libx265-dev libfdk-aac-dev libmp3lame-dev libopus-dev libvpx-dev
+#install NASM assembler
+cd ~/ffmpeg_sources
+wget http://www.nasm.us/pub/nasm/releasebuilds/2.13.01/nasm-2.13.01.tar.bz2
+tar xjvf nasm-2.13.01.tar.bz2
+cd nasm-2.13.01
+./autogen.sh
+PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
+PATH="$HOME/bin:$PATH" make
+make install
+
+#compile x264... 
+cd ~/ffmpeg_sources
+wget http://download.videolan.org/pub/x264/snapshots/last_x264.tar.bz2
+tar xjvf last_x264.tar.bz2
+cd x264-snapshot*
+PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static --disable-opencl
+PATH="$HOME/bin:$PATH" make
+make install
 
 #install x265 manually - ffmpeg seems to fail in finding the ubuntu apt install of x265
 sudo apt-get install cmake mercurial
