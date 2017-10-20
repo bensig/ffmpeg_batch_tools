7e55f54c8d91818051e3edc21571408460dbd2bc
Ben Sigman <bensig@gmail.com>
2017-10-20 18:07:21 -0400

    adding dnxhd conversion and batch conversion scripts

---
+++ convert-dnxhd-1080p.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#This script will burn timecode from what is embedded in a video file
+#in_file_path=/path/to/input_file
+read -e -p "Enter path to input file:" in_file_path
+echo -e "$in_file_path"
+#out=/path/to/ouput_file.mov
+out_file_path="${in_file_path%.*}_dnxhd.mov"
+echo -e "$out_file_pah"
+#TC rate should be identical to FPS
+timecode_rate=24
+#scaling
+video_resolution_scaling="scale=1920x1080,fps=30000/1001"
+video_endcoder='-c:v dnxhd'
+#remove audio
+audio_encoder_option='-an'
+# And finally run the ffmpeg script
+#ffmpeg -threads 0 -i $in_file_path $audio_encoder_option $video_endcoder $video_variable_bitrate -preset $video_encoder_preset -deinterlace -vf "$video_resolution_scaling" $out_file_path
+ffmpeg -threads 0 -i $in_file_path $audio_encoder_option -vf format=yuv422p10,scale=1920x1080 -r 24000/1001 -c:v dnxhd -b:v 175M $out_file_path
